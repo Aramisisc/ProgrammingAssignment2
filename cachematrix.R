af46523fa773bf8ea8fd15c4fe882c4b82e96175
Antonio <aramisisc@belgacom.net>
2014-06-22 15:24:45 +0200

    Submitting my changes for assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+#create a function that creates a first special object that stores a matrix 
+# and cache's its inverse.
+#The first function, makeCacheMatrix creates a special "vector", which is really 
+#a list containing a function to:
+#-set the value of the matrix
+#-get the value of the matrix
+#-set the the inverse matrix
+#-get the inverse matrix
+#We assume that the provided matrix is a square invertible matrix 
+#f.i.: matrix(1:4,2,2)
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix())  {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+#The second function,cacheSolve(), calculates the inverse of the matrix that 
+#has been created with the makeCacheMatrix function. 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse() #checks to see if the inverser has already been 
+                            #calculated
+        if(!is.null(m)) {  #If so, it gets the inverse from the cache and skips
+                           #the computation
+                message("getting cached data") 
+                return(m)
+        }
+        data <- x$get()  #Otherwise, it calculates the inverse of the matrix... 
+        m <- solve(data, ...)
+        x$setinverse(m)  #..and sets the value of the inverse in the cache via 
+                         #the setinverse function
+        m
 }
